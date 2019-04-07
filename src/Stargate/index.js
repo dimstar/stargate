f47e41d4071a0277314ab6a43663ca695b01d41a
dimstar <kylermagee@gmail.com>
2019-04-07 11:47:17 -0700

    keyframe animation for background image
    only did left side animation example

--- src/Stargate/index.js
+++ src/Stargate/index.js
@@ -1,23 +1,50 @@
 import React from 'react';
 import StarMask from './StarMask';
-// import Stars from './Stars';
+import Splat from './Splat';
 import './Stargate.css';
 
+class Stargate extends React.Component {
+  constructor() {
+    super();
 
-const Stargate = () => {
-  return (
-    <>
-        <div className="stargate">
-          <div className="occular">
-            <div className="left"></div>
-            <div className="right"></div>
+    this.state = {
+      reversed_right: false,
+      reversed_left: 'reversed',
+    }
+  }
+
+  reversable = (delay) => {
+    setInterval(() => {
+      this.setState( (state)=> {
+        return {
+          reversed_right: (state.reversed_right === false) ? 'reverse' : false,
+          reversed_left: (state.reversed_left === false) ? 'reverse' : false,
+        }
+      });
+    }, delay);
+  };
+
+  componentDidMount() {
+    this.reversable(6000);
+  }
+
+  render() {
+    return (
+      <>
+          <div className="stargate">
+            <div className="occular">
+              <div className={`left ${this.state.reversed_right}`}>
+                <Splat></Splat>
+              </div>
+              <div className={`right ${this.state.reversed_right}`}></div>
+            </div>
           </div>
+        <div className='gate-wrapper'>
+          <StarMask></StarMask>
         </div>
-      <div className="gate-wrapper">
-        <StarMask></StarMask>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default Stargate;
